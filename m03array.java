16c0401fd4c55ef1aac182bc28b52b6cee35ea53
aantonimoran <antonimoran@iesmontsia.org>
2022-02-03 03:34:35 -0500

    Variable contador

---
+++ m03array.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class m03array {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+        int i = 0;
+        int [] nums = new int[6];
+        System.out.println("Escriu 6 numeros: ");
+        while (i < 6) {
+            nums[i] = sc.nextInt();
+            i++;
+        }
+        System.out.println("Escriu el numero a adivinar: ");
+        int numadivinar = sc.nextInt();
+        i = 0;
+        while (i < 6) {
+            if (nums[i]== numadivinar) {
+                System.out.println("S'ha trobat el numero a la posicio " + (i + 1));  
+                break;
+            }
+            i++;
+        }
+        if (i == 7) {
+            System.out.println("No s'ha trobat el numero");
+        }
+    }
+}
